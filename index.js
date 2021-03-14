92a4039815d37a6e83f9b98e7e5f5ff981148150
Juju Man <hunteripad2@gmail.com>
2021-03-14 18:36:26 +0300

    Added heroku database

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const { Pool } = require("pg");
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
@@ -10,6 +18,17 @@ app.get("/todoData", (req, res) => {
         "Access-Control-Allow-Origin": clientUrl,
     });
     res.json(todoData);
+}).get("/db", async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query("SELECT * FROM test_table");
+        const results = { results: result ? result.rows : null };
+        res.render("pages/db", results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
 });
 
 app.post("/todoData", (req, res) => {
@@ -17,12 +36,34 @@ app.post("/todoData", (req, res) => {
         "Access-Control-Allow-Origin": clientUrl,
     });
     //todoData.push(JSON.parse(req.body));
+}).get("/db", async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query("SELECT * FROM test_table");
+        const results = { results: result ? result.rows : null };
+        res.render("pages/db", results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
 });
 
 app.options("/todoData", (req, res) => {
     res.set({
         "Access-Control-Allow-Origin": clientUrl,
     });
+}).get("/db", async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query("SELECT * FROM test_table");
+        const results = { results: result ? result.rows : null };
+        res.render("pages/db", results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
 });
 
 app.listen(port);
